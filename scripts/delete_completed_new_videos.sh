f5dac5fcdfc0ca2993041ca1203630d20799a7d0
Tatsuya Miyado <miyado@gmail.com>
2015-10-04 04:31:18 +0900

    Update delete_completed_new_videos.sh

--- scripts/delete_completed_new_videos.sh
+++ scripts/delete_completed_new_videos.sh
@@ -1,8 +1,8 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
 if [ "$MYSQL_HOGESUKE_PASS" = "" ]; then
     echo "Please set MYSQL_HOGESUKE_PASS to the environment variable."
     exit 1
 fi
 
-mysql -u hogesuke -p $MYSQL_HOGESUKE_PASS skimii -e "DELETE FROM new_videos WHERE status = 1;"
+mysql -u hogesuke -p$MYSQL_HOGESUKE_PASS nicotune -e "DELETE FROM new_videos WHERE status = 1;"
