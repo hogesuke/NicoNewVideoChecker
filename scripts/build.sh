06d7ec3f9bee003d63dbed3085de356e21521a43
hogesuke <miyado@gmail.com>
2015-11-08 02:52:15 +0900

    build.sh: go buildの出力先を指定するように修正（その2）

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,5 +2,5 @@
 
 cd `dirname $0`
 
-GOOS=linux GOARCH=amd64 go build -o ../src/ ../src/NewVideoCollector.go
-GOOS=linux GOARCH=amd64 go build -o ../src/ ../src/VideoAnalyzer.go
+GOOS=linux GOARCH=amd64 go build -o ../src/NewVideoCollector ../src/NewVideoCollector.go
+GOOS=linux GOARCH=amd64 go build -o ../src/VideoAnalyzer ../src/VideoAnalyzer.go
